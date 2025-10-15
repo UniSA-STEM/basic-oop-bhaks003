71d076c6af0ab114867ed884438be96e4b74cd15
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-10-15 15:06:47 +1030

    created a asset class with the attributes in which one works on boolean expression and getters to name and description

--- Asset.py
+++ Asset.py
@@ -6,3 +6,15 @@ ID: 110429045
 Username: bhaks003
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class asset:
+    def __init__(self, name, description, encrypted = False):
+        self.__name = name
+        self.__description = description
+        self.__encrypted = encrypted
+
+    def get_name(self):
+        return self.__name
+
+    def get_description(self):
+        return self.__description
