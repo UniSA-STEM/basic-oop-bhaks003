f73120265ea80f3c5119f28db480b69eb67bd44b
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-10-19 19:07:26 +1030

    created a basic title header for the py test

--- Main.py
+++ Main.py
@@ -6,3 +6,15 @@ ID: 110429045
 Username: bhaks003
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+
+from Hacker import Hacker
+from Asset import asset
+from Rig import Rig
+
+def print_head(title):
+    print()
+    print(85*'-')
+    print(20*"-",title,20*"-")
+    print(85*'-')
+
