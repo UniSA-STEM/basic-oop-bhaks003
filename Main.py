9f3bd7116962a43d5361efd091b4f5d5ac3ef063
bhaks003 <bhaks003@mymail.unisa.edu.au>
2025-10-19 19:19:26 +1030

    test info containing about encryption and decryption

--- Main.py
+++ Main.py
@@ -19,16 +19,38 @@ def print_head(title):
     print(85*'-')
 
 def test_acquire_and_upgrade_rig():
+    # Setup
     print_head("Creating Hackers, rigs and few assets")
+    # assigning hacker
     hacker_1 = Hacker("JackSparrow")
     hacker_2 = Hacker("Neon")
 
+    # hacker acquiring rig
     hacker_1.acquire_rig("TheBlackPearl")
     hacker_2.acquire_rig("TheBlackSuit")
 
+    # creating a seperate rig to use in battles
     Rig_1 =  Rig("TheBlackPearl")
     Rig_2 = Rig("TheBlackSuit")
 
     print(hacker_1)
     print(hacker_2)
     print(Rig_1)
+
+    # storing and transfering invertory to storage
+    print_head("Storing and Retrieving Assets")
+    # to retrieve asset from the attacker rig
+    hacker_1.retrieve_asset("Removable Drive")
+    hacker_1.store_asset("Removable Drive")
+    print(hacker_1)
+
+    # test incudes about encrpytion and decrption as trying without a security chip
+    print_head("Trying encryption with out security chip")
+    hacker_1.retrieve_asset("Data Spike") #getting one into inventory to attempt encryptiomn
+    hacker_1.encrypt_inventory("Data Spike") # expected to be fail no chip found
+    hacker_1.decrypt_inventory("Data Spike") # expected output would be it is already decrypyted
+
+    # now we will try on encrypt ands decrypt in the rig without a chip
+    hacker_1.store_asset("Data Spike")
+    hacker_1.encrypt_rig("Data Spike")
+    hacker_1.decrypt_rig("Data Spike")
